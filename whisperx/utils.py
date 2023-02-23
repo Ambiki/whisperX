7b8eb210edb10aa53c9721d9bb2bb3a5d726ae3f
dev-ham <devhammad.masood@gmail.com>
2023-02-23 16:10:00 +0500

    Added speaker for segments

--- whisperx/utils.py
+++ whisperx/utils.py
@@ -87,11 +87,12 @@ def write_srt(transcript: Iterator[dict], file: TextIO):
     """
     for i, segment in enumerate(transcript, start=1):
         # write srt lines
+        speaker = f"[{segment['speaker']}]: " if 'speaker' in segment else ''
         print(
             f"{i}\n"
             f"{format_timestamp(segment['start'], always_include_hours=True, decimal_marker=',')} --> "
             f"{format_timestamp(segment['end'], always_include_hours=True, decimal_marker=',')}\n"
-            f"{segment['text'].strip().replace('-->', '->')}\n",
+            f"{speaker}{segment['text'].strip().replace('-->', '->')}\n",
             file=file,
             flush=True,
         )
